8d109707ba0bda1876927cac8d78cd10d90382f4
Plotnikov-Alexey <aleksei.plotniko@mail.ru>
2022-09-11 21:16:22 +0300

    17 point

--- script.js
+++ script.js
@@ -24,3 +24,29 @@ var titleCaseOver;
 studlyCapVar = 10;
 properCamelCase = "A String";
 titleCaseOver = 9000;
+
+let catName = "Oliver";
+let catSound = "Meow!";
+
+const FCC = "freeCodeCamp"; // Change this line
+let fact = "is cool!"; // Change this line
+fact = "is awesome!";
+console.log(FCC, fact); // Change this line
+
+const sum = 10 + 5;
+const sum = 10 - 5;
+const product = 8 * 10;
+const quotient = 66 / 33;
+let myVar = 87;
+
+// Only change code below this line
+myVar++;
+let myVar = 11;
+
+// Only change code below this line
+myVar--;
+const ourDecimal = 5.7;
+
+// Only change code below this line
+let myDecimal =5.7;
+const product = 2.0 * 2.5;
